ee1bf0423c82349b2a1ec3d4b73df7a1faf8d32b
Nataliia Stefurak <stefuraknataliia@gmail.com>
2018-06-29 00:17:27 -0400

    route files added

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
 const app = express();
 
 //DB Config
@@ -13,6 +17,11 @@ mongoose
   .catch(error => console.log(`Error: ${error}`));
 
 app.get("/", (req, res) => res.send("Hello1"));
+
+//use routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
