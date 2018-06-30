9ac24c378d8d258b5c3cd280d6dc1d08ea72b45f
Nataliia Stefurak <stefuraknataliia@gmail.com>
2018-06-29 20:08:24 -0400

    User Registration created

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-const express = require("express");
-const mongoose = require("mongoose");
-
-const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //DB Config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
 //Connect to MongoDB
 mongoose
   .connect(db)
-  .then(() => console.log("MongoDB connected"))
+  .then(() => console.log('MongoDB connected'))
   .catch(error => console.log(`Error: ${error}`));
 
-app.get("/", (req, res) => res.send("Hello1"));
+app.get('/', (req, res) => res.send('Hello1'));
 
 //use routes
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
