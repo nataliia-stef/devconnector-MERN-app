38892aab7d9e6faa41e356907391a9635d09995d
Nataliia Stefurak <stefuraknataliia@gmail.com>
2018-07-05 13:56:07 -0400

    Experience route with field validation created

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 
 //load Validation
 const validateProfileInput = require('../../validation/profile');
+const validateExperienceInput = require('../../validation/experience');
 
 //load Profile Model
 const Profile = require('../../models/Profile');
@@ -178,6 +179,40 @@ router.post(
             })
         );
       }
+    });
+  }
+);
+
+//@route   POST api/profile/experience
+//@desc    Add experience to profile
+//@acess   Private
+
+router.post(
+  '/experience',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateExperienceInput(req.body);
+
+    //Check validation
+    if (!isValid) {
+      //Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const newExp = {
+        title: req.body.title,
+        company: req.body.company,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      };
+
+      //Add to experience to the array and return the new profile
+      profile.experience.unshift(newExp);
+
+      profile.save().then(profile => res.json(profile));
     });
   }
 );
