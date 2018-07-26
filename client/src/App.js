5ad174531b8a068e8191fd23c7f09caa3d4ab990
Nataliia Stefurak <stefuraknataliia@gmail.com>
2018-07-26 19:38:03 -0400

    NotFound Component created

--- client/src/App.js
+++ client/src/App.js
@@ -25,6 +25,7 @@ import EditProfile from './components/edit-profile/EditProfile';
 import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import NotFound from './components/not-found/NotFound';
 
 //Check for token
 if (localStorage.jwtToken) {
@@ -83,6 +84,7 @@ class App extends Component {
                 />
                 <Route exact path="/profiles" component={Profiles} />
                 <Route exact path="/profile/:handle" component={Profile} />
+                <Route exact path="/not-found" component={NotFound} />
               </Switch>
             </div>
             <Footer />
